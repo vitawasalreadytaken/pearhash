f6f0ae105c6950fe69573fde4f0fcbbadd85ab1b
Vita Smid <me@ze.phyr.us>
2014-12-14 10:31:02 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = 'pearhash',
+	version = '0.1.0',
+	author = 'Vita Smid',
+	author_email = 'me@ze.phyr.us',
+	py_modules = ['pearhash',],
+	url = 'https://github.com/ze-phyr-us/pearhash',
+	license = 'LICENSE',
+	description = 'Pearson hashing algorithm'
+)
